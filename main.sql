fc7e8d8e31fc08aa03efe6c8f812ef1a1e9e7a8c
Sukhendra <developer.sukhendra@gmail.com>
2026-07-07 16:41:14 +0530

    subquery added for getting all the students that have number greater than average and less than average

--- main.sql
+++ main.sql
@@ -1,1 +1,20 @@
 SELECT * FROM students;
+SELECT * FROM exam_scores;
+SELECT * FROM projects;
+
+
+SELECT AVG(score) AS class_avg_score 
+FROM exam_scores;
+
+SELECT 
+s.name AS student_name,
+s.branch AS student_branch,
+e.score AS student_score,
+e.subject AS student_subject
+FROM exam_scores AS e
+INNER JOIN students AS s 
+ON s.student_id = e.student_id
+WHERE e.score < (
+SELECT AVG(score) AS class_average 
+FROM exam_scores
+);
